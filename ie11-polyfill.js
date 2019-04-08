709d8c4b699843132266f91618e356500aceba49
sadfsdf79ss4 <sadfsdf79ss4@gmail.com>
2019-04-09 08:01:04 +0900

    인터넷 익스플로어 호환성 개선

---
+++ ie11-polyfill.js
@@ -0,0 +1,31 @@
+//URLSearchParams polyfill
+(function (w) {
+
+    w.URLSearchParams = w.URLSearchParams || function (searchString) {
+        this.searchString = searchString;
+        this.get = function (name) {
+            var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(this.searchString);
+            if (results == null) {
+                return null;
+            }
+            else {
+                return decodeURI(results[1]) || 0;
+            }
+        };
+    }
+
+})(window);
+
+//querystring encoder [href] attribute
+//save onload handler
+var windowOnLoad = window.onload;
+window.onload = function() {
+    //if saved onload handler exists, execute it.
+    windowOnLoad && windowOnLoad();
+    var target = document.querySelectorAll('[href]');
+    for(var i = 0;i < target.length;i++) {
+        var url = target[i].getAttribute('href');
+        url = encodeURI(url);
+        target[i].setAttribute('href', url);
+    }
+}
