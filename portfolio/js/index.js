95ff6a53868652b75f89a782ad44e933d4107f53
sadfsdf79ss4 <sadfsdf79ss4@gmail.com>
2019-05-23 15:30:13 +0900

    포토폴리오 스크롤 애니메이션 모바일 브라우저 호환성 개선

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -65,46 +65,42 @@ let cancelScrollAnimation;
 let scrollTimeStart;
 let scrollLength;
 let scrollStart;
-const rootElement = document.querySelector("html");
-function animateScroll(
-    target,
-    animationTime,
-    easingFunc
-) {
+function animateScroll(target, animationTime, easingFunc) {
     animationTime = animationTime || 1500;
     easingFunc = easingFunc || EasingFunctions.easeInOutCubic;
     let targetPosition;
-    if(typeof target !== 'number') {
+    if (typeof target !== "number") {
         //target이 선택자라고 가정
         let element = document.querySelector(target);
         targetPosition = getScrollPosition(element);
     } else {
         targetPosition = target;
     }
-    if(scrollAnimating) {
+    if (scrollAnimating) {
         cancelScrollAnimation = true;
-        while(!scrollAnimating);
+        while (!scrollAnimating);
     }
     function step(time) {
         //스크롤 시작시간과 시작위치 초기화
-        if(!scrollTimeStart) {
+        if (!scrollTimeStart) {
             scrollTimeStart = time;
-            scrollStart = rootElement.scrollTop;
+            scrollStart = getScrollTop();
             scrollLength = targetPosition - scrollStart;
             scrollAnimating = true;
         }
         //현재 진행률 계산
         let progress = (time - scrollTimeStart) / animationTime;
         //애니메이션 종료조건 체크
-        if(cancelScrollAnimation || progress > 1) {
+        if (cancelScrollAnimation || progress > 1) {
             cancelScrollAnimation = false;
             scrollAnimating = false;
             scrollTimeStart = undefined;
             return;
         }
+        //alert(getScrollTop());
         //애니메이션된 스크롤 위치 계산
         let animatedPos = easingFunc(progress) * scrollLength + scrollStart;
-        rootElement.scrollTop = animatedPos;
+        setScrollTop(animatedPos);
         //다음 애니메이션 스케줄링
         window.requestAnimationFrame(step);
     }
@@ -112,12 +108,32 @@ function animateScroll(
 }
 
 function scrollToBottom() {
-    animateScroll(rootElement.scrollHeight);
+    animateScroll(getScrollHeight());
+}
+
+//안드로이드 웹뷰 등 기타의 브라우저에서 scrollTop 안되는 현상 수정
+const rootElement = document.querySelector("html");
+//https://stackoverflow.com/questions/871399/cross-browser-method-for-detecting-the-scrolltop-of-the-browser-window
+function getScrollingElementProperty(p) {
+    return rootElement[p] || document.body[p];
+}
+function setScrollingElementProperty(p, d) {
+    rootElement[p] = document.body[p] = d;
+}
+function getScrollTop() {
+    return getScrollingElementProperty("scrollTop");
+}
+
+function setScrollTop(d) {
+    setScrollingElementProperty("scrollTop", d);
+}
+
+function getScrollHeight() {
+    return getScrollingElementProperty("scrollHeight");
 }
 
 function getScrollPosition(targetElement) {
     let bcr = targetElement.getBoundingClientRect();
     let elemOffset = "y" in bcr ? bcr.y : bcr.top;
-    return rootElement.scrollTop + elemOffset;
+    return getScrollTop() + elemOffset;
 }
-
